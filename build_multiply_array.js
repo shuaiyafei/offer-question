c6624fb93219f7d0cec3d923e969d7e04c798183
yafei shuai <yafeishuai@tencent.com>
2020-04-21 20:41:05 +0800

    feat

--- build_multiply_array.js
+++ build_multiply_array.js
@@ -10,3 +10,19 @@ const multiply = (array) => {
   }
   return result
 }
+
+const multiply = (array) => {
+  const length = array.length
+  if (length == 0) return []
+  const result = new Array(length)
+  result[0] = 1
+  for (i = 1; i < length; i++) {
+    result[i] = array[i - 1] * result[i - 1]
+  }
+  let tmp = 1
+  for (i = length - 2; i >= 0; i--) {
+    tmp *= array[i + 1]
+    result[i] *= tmp
+  }
+  return result
+}
